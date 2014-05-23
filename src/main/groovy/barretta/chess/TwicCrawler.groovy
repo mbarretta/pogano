4720013a927e6fe143c2354a1cbe59ca80adcc9b
Mike Barretta <mike.barretta@gmail.com>
2014-05-22 20:25:04 -0400

    right...made it actually run

--- src/main/groovy/barretta/chess/TwicCrawler.groovy
+++ src/main/groovy/barretta/chess/TwicCrawler.groovy
@@ -7,7 +7,6 @@ import org.ccil.cowan.tagsoup.Parser
 @Slf4j
 class TwicCrawler {
     def config = new ConfigSlurper().parse(new File("twic.properties").toURI().toURL())
-    def count = 0
 
     public static void main(String[] args) {
         new TwicCrawler().run()
@@ -17,39 +16,50 @@ class TwicCrawler {
         assert config.zipsUrl, "url must be set"
         assert config.pgnFile, "pgnFile must be set"
 
-        log.info("Starting...\nwill append to [${config.pgnFile}]")
+        log.info("Starting...will append to [${config.pgnFile}]")
 
         if (config.fetchHistory) {
-            def startId = findFirstId()
-            while (true) {
-                try {
-                    appendPgns(fetchAndExtractPgns(startId++))
-                    log.info("pulled [$count] zips")
-                } catch (IOException e) {
-                    log.info("Last id found [${startId - 1}]")
-                    break
-                }
-            }
+            fetchHistory()
         } else {
-            startId = startId ?: findLastId()
-            try {
-                appendPgns(fetchAndExtractPgns(startId))
-            } catch (IOException e) {
-                log.error("unable to find file with id [$startId]")
-            }
+            fetchId()
         }
 
-        saveLastId(startId)
+        log.info("Done")
+    }
+
+    def fetchId() {
+       def startId = config.fetchId ?: findLastId()
+        try {
+            appendPgns(fetchAndExtractPgns(startId))
+            saveLastId(startId)
+        } catch (IOException e) {
+            log.error("unable to find file with id [$startId]")
+        }
+    }
+
+    def fetchHistory() {
+        def startId = findFirstId()
+        def count = 0
+        while (true) {
+            try {
+                appendPgns(fetchAndExtractPgns(startId++))
+                saveLastId(startId)
+                count++
+            } catch (IOException e) {
+                log.info("Last id found [${startId - 1}]")
+                break
+            }
+        }
+        log.info("Pulled [$count] zips")
     }
 
     def fetchAndExtractPgns(id) {
         def address = "${config.zipsUrl}/twic${id}g.zip"
-        log.trace("fetching [$address]")
+        log.info("Fetching [$address]")
 
         def zipStream = new GroovyZipInputStream(new URL(address).openStream())
         def entries = []
         zipStream.eachEntry { zip ->
-            count++
             def entry = new StringBuilder()
             zip.eachLine {
                 entry.append(it).append("\n")
@@ -61,15 +71,16 @@ class TwicCrawler {
 
     def findLastId() {
         def lastId = getZipList().max()
-        log.info("found latest file id [$lastId]")
+        log.info("Found latest file id [$lastId]")
         return lastId
     }
 
     def findFirstId() {
         def firstId = getZipList().min()
-        log.info("found earliest file id [$firstId]")
+        log.info("Found earliest file id [$firstId]")
+        return firstId
     }
- 
+
     def getZipList() {
         def html = new XmlSlurper(new Parser()).parse(new URL(config.zipsUrl).openStream())
         return html.body.ul.li.inject([]) { list, file ->
@@ -79,17 +90,16 @@ class TwicCrawler {
                 list << (matcher.group() as int)
             }
             return list
-	}
-
+        }
     }
 
     def appendPgns(pgns) {
-        log.trace("writing to [${config.pgnFile}]")
+        log.trace("Writing to [${config.pgnFile}]")
         new File(config.pgnFile) << pgns.join("\n")
     }
 
     def saveLastId(id) {
-        config.startId = ++id
+        config.fetchId = ++(id as int)
         new File("twic.properties").withWriter {
             config.writeTo(it)
         }
